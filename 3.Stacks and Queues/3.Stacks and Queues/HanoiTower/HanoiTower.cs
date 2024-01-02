4587c5c3567eff39eae1af22d8bcfd34f3a72420
speris <speris@grupokh.com>
2024-01-02 15:45:17 +0100

    02.01 - Added class HanoiTower in 3.Stacks and Queues project.

---
+++ 3.Stacks and Queues/3.Stacks and Queues/HanoiTower/HanoiTower.cs
@@ -0,0 +1,13 @@
+﻿namespace _3.Stacks_and_Queues.HanoiTower
+{
+    public class HanoiTower
+    {
+        public int DiscsCount { get; private set; }
+        public int MovesCount { get; private set; }
+        public Stack<int>? From { get; private set; }
+        public Stack<int>? To { get; private set; }
+        public Stack<int?>? Auxiliary { get; private set; }
+
+        public event EventHandler<EventArgs>? MoveCompleted;
+    }
+}
