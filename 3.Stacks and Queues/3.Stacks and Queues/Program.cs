cfe1bc3cbb36191515c8b8d92fe0097c1c4a4ebc
SalvaPeris <sperisgimeno@gmail.com>
2024-04-01 19:37:08 +0200

    Added constants.

--- 3.Stacks and Queues/3.Stacks and Queues/Program.cs
+++ 3.Stacks and Queues/3.Stacks and Queues/Program.cs
@@ -1,5 +1,9 @@
 ﻿internal class Program
 {
+    private const int DISCS_COUNT = 10;
+    private const int DELAY_MS = 250;
+    private static int _columnSize = 30;
+
     private static void Main(string[] args)
     {
         
