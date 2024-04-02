f5334c7acc2a208347a31bd60e9fb4e4302f7280
SalvaPeris <sperisgimeno@gmail.com>
2024-04-02 20:47:05 +0200

    Added Hanoi Tower algorithm pattern.

--- 3.Stacks and Queues/3.Stacks and Queues/Program.cs
+++ 3.Stacks and Queues/3.Stacks and Queues/Program.cs
@@ -10,5 +10,17 @@ internal class Program
         algorithm.MoveCompleted += algorithm.AlgorithmVisualize;
         algorithm.AlgorithmVisualize(algorithm, EventArgs.Empty);
         algorithm.Start();
+
+        Move(DISCS_COUNT, "F", "T", "A");
+    }
+
+    public static void Move(int discs, string from, string to, string auxiliary)
+    {
+        if (discs > 0)
+        {
+            Move(discs - 1, from, auxiliary, to);
+            Console.WriteLine(from + " -> " + to);
+            Move(discs - 1, auxiliary, to, from);
+        }
     }
 }
