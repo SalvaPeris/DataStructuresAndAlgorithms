6e3a97230a55ddd5bee72e3915cd33a17660a143
SalvaPeris <sperisgimeno@gmail.com>
2024-04-03 22:19:11 +0200

    Added CallCenter example.

---
+++ 3.Stacks and Queues/3.Stacks and Queues/CallCenter/IncomingCall.cs
@@ -0,0 +1,12 @@
+﻿namespace _3.Stacks_and_Queues.CallCenter
+{
+    public class IncomingCall
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public DateTime CallTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? Consultant { get; set; }
+    }
+}
