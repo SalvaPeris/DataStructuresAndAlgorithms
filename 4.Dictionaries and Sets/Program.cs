f30c31013604902eaaa0787c08d38f07007731fa
SalvaPeris <sperisgimeno@gmail.com>
2024-07-22 14:07:21 +0200

    Added Chapter 4.

--- 4.Dictionaries and Sets/Program.cs
+++ 4.Dictionaries and Sets/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Hashtable phoneBook = new Hashtable()
+        {
+            {"SalvaPeris", "000-000-000"},
+            {"PepeMartinez", "111-111-111"}
+        };
+    }
+}
