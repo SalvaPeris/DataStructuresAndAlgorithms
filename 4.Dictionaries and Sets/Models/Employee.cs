2395fee02cf68cc517b884e719d85f7636bbc0f9
Salvador Peris Gimeno <79948536+SalvaPeris@users.noreply.github.com>
2024-07-29 10:02:21 +0200

    Added User details example.

---
+++ 4.Dictionaries and Sets/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace _4.Dictionaries_and_Sets.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- 4.Dictionaries and Sets/Program.cs
+++ 4.Dictionaries and Sets/Program.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using _4.Dictionaries_and_Sets.Models;
+using System.Collections;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    /*private static void Main(string[] args)
     {
         Hashtable phoneBook = new Hashtable()
         {
             {"SalvaPeris", "000-000-000"},
             {"PepeMartinez", "111-111-111"}
         };
+    }*/
+
+    private static void Main(string[] args)
+    {
+        Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+        employees.Add(1, new Employee() { FirstName = "Salva", LastName = "Peris", PhoneNumber = "000-111-222"});
+        employees.Add(1, new Employee() { FirstName = "Pepe", LastName = "Martínez", PhoneNumber = "000-111-333" });
+
     }
 }
