54bd5caf2192115c07802040f224f171ed60db18
SalvaPeris <sperisgimeno@gmail.com>
2024-10-21 09:54:06 +0200

    Added Chapter 4 - Remove duplicates.

---
+++ 4.Dictionaries and Sets - Duplicates/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is a demo for removing duplicates: ");
+
+        List<string> names = new List<string>()
+        {
+            "Salva",
+            "Mario",
+            "Maria",
+            "Pepe",
+            "Alberto",
+            "Emilio",
+            "mario",
+            "Jaime",
+            "salva"
+        };
+        SortedSet<string> sorted = new SortedSet<string>(
+            names,
+            Comparer<string>.Create((a, b) => a.ToLower().CompareTo(b.ToLower())));
+
+        foreach (string name in sorted)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
