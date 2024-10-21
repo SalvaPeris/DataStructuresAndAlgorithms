f82b3b910435f4b89d1867d4b887a5da9279cef3
SalvaPeris <sperisgimeno@gmail.com>
2024-10-21 16:42:47 +0200

    Added Chapter 5 - AVL tree.

---
+++ 5.Trees - AVL/Program.cs
@@ -0,0 +1,18 @@
+﻿using SelfBalancedTree;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is a demo for keeping the tree balanced:");
+
+        AVLTree<int> tree = new AVLTree<int>();
+        for (int i = 1; i < 10; i++)
+            tree.Add(i);
+
+        Console.WriteLine("Get Height : " + string.Join(", ", tree.GetHeightLogN()));
+        Console.WriteLine("Descending : " + string.Join(", ", tree.ValuesCollectionDescending));
+        Console.WriteLine("Printing AVL tree:");
+        tree.Print();
+    }
+}
