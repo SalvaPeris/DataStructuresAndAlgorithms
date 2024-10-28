904017103403765afb6f507c6fd21e030de05293
SalvaPeris <sperisgimeno@gmail.com>
2024-10-28 10:41:57 +0100

    Added Telecom cable exaample project.

---
+++ 6.Graphs - Telecommunication cable/Program.cs
@@ -0,0 +1,7 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is the telecommunication cable example using Kruskal's and Prim's algorithms: ");
+    }
+}
